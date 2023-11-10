b17d1f6843aa983d687df456b72ffe176247688d
anoj-roshan <s223441955@deakin.edu.au>
2023-11-10 11:28:37 +1100

    done

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+function changeText() {
+    // this should change the displlay text
+    let textList = ['Hola','Bonjour','Vanakka','Namaste','wassup'];
+    let randomNumber = getRandomNumber(0, textList.length - 1);
+    console.log(randomNumber);
+    document.getElementById('header').innerHTML = textList[randomNumber];
+}
+
+function getRandomNumber(min,max) {
+    // generate a random number between a range
+    return Math.floor(Math.random()*(max-min + 1)+min);
+}
